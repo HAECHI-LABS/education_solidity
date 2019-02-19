53651a687e872a5d0690bd37042de3d6662b8571
leekt <leekt216@gmail.com>
2019-02-19 17:28:39 +0900

    ERC20 skeleton

---
+++ contracts/ERC20.sol
@@ -0,0 +1,32 @@
+pragma solidity ^0.4.24;
+
+import "./IERC20.sol";
+
+contract ERC20 is IERC20 {
+    constructor () public {
+    }
+
+    function transfer(address to, uint256 value) external returns (bool) {
+        
+    }
+
+    function approve(address spender, uint256 value) external returns (bool) {
+
+    }
+
+    function transferFrom(address from, address to, uint256 value) external returns (bool) {
+        
+    }
+
+    function totalSupply() external view returns (uint256) {
+
+    }
+
+    function balanceOf(address who) external view returns (uint256) {
+
+    }
+
+    function allowance(address owner, address spender) external view returns (uint256) {
+
+    }
+}
