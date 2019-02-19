647c237b29a8a6f11fad0b3e364d4459208ca2b9
leekt <leekt216@gmail.com>
2019-02-19 17:29:58 +0000

    Ownable

---
+++ contracts/ERC20Burnable.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.4.24;
+
+import "./ERC20Mintable.sol";
+import "./IERC20Burnable.sol";
+
+contract ERC20Burnable is ERC20Mintable, IERC20Burnable {
+    function burn(uint256 value) public returns (bool){
+
+    }
+
+    function burnFrom(address owner, uint256 value) public returns(bool) {
+
+    }
+}
---
+++ contracts/ERC20Mintable.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.4.24;
+
+import "./ERC20.sol";
+import "./IERC20Mintable.sol";
+import "./Ownable.sol";
+
+contract ERC20Mintable is ERC20, IERC20Mintable, Ownable {
+    function mint(address to, uint256 value) public onlyOwner returns (bool){
+
+    }
+}
